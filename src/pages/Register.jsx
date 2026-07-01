a91bfdbb3333459bbd2b633b7ca7a93a87e2398c
Nathan e Renan <0001151944@senaimgaluno.com.br>
2026-07-01 17:21:09 -0300

    v4

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Logo } from "../ui/Logo";
+import { Logo } from "../components/layout/ui/Logo";
 
 // CPF validation algorithm
 function validateCPF(cpf) {
